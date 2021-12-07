1da9cd419945afed72cd6ecff5b1d6fbc9dc778e
Elias4234 <reddawn4234@gmail.com>
2021-12-07 03:22:01 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
+import {useState, useEffect} from 'react';
+import {baseDeDato} from './ConfiguracionFirebase.js';
+import UsuarioNuevo from "./UsuarioNuevo.js";
+import Usuario from "./Usuario.js";
 import "./App.css";
-import "./Inicio.js";
 
-class App extends Component{
+function App() {
+ Const [contactos, setContactos] = useState([]);
+
+ function agregarNuevoContacto(contacto){
+  cargarContactos();
+ }
+ function cargarContactos(){
+  const listado = [];
+  baseDeDato.collection('contactos').get()
+  .then(resultado =>{
+   resultado.forEach(contacto => {
+     listado.push(contacto.data());
+    })
+   setContactos(listado);
+ }).catch(error => console.log(error));
+ }
+ 
+ useEffect(cargarContactos,[]);
   
-  render(){
-    return 
-     
-  }
+ return (
+    <div className = "App">
+      <header className = "App-header">
+        <ContactoNuevo agregarNuevoContacto={agregarNuevoContacto} />
+        {
+          contactos && contactos.length && contactos.slice().reverse().map((contacto,i) => {
+            const {nombre, apellido, telefono} = contacto;
+            return (<Contacto key = {i} nombre ={nombre} apellido = {apellido} telefono = {telefono}/>)
+          })
+        }
+      </header>
+    </div>
+  );
 }
 
 export default App;
